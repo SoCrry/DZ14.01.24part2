9dc2faea47d626e324f3b5e97a19a129d107b607
Karen.P <karen-pashoyan@mail.ru>
2024-01-18 17:27:25 +0500

    Changes fun

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 package ru.netology
 
 fun main() {
-    var amount = 15000
+    var amount = 14000
     var typeCard = "VK Pay"
     var previousTransfer = 0
 
@@ -11,8 +11,16 @@ fun main() {
 
     println()
 
-    amount = 10000
-    typeCard = "Maestro"
+    amount = 15000
+    typeCard = "VK Pay"
+    resultTransfer(amount, typeCard, previousTransfer)
+    previousTransfer += resultTransfer(amount, typeCard, previousTransfer).toInt()
+    resultPrint(amount, typeCard, previousTransfer)
+
+    println()
+
+    amount = 11000
+    typeCard = "VK Pay"
     resultTransfer(amount, typeCard, previousTransfer)
     previousTransfer += resultTransfer(amount, typeCard, previousTransfer).toInt()
     resultPrint(amount, typeCard, previousTransfer)
@@ -20,7 +28,7 @@ fun main() {
     println()
 
     amount = 100000
-    typeCard = "Mastercard"
+    typeCard = "Maestro"
     resultTransfer(amount, typeCard, previousTransfer)
     previousTransfer += resultTransfer(amount, typeCard, previousTransfer).toInt()
     resultPrint(amount, typeCard, previousTransfer)
@@ -29,11 +37,12 @@ fun main() {
 }
 
 fun typeMMCommission(amount: Int, previousTransfer: Int): Double {
-    val commission = if (amount > 75000) {
+    var commission = if (amount > 75000) {
         amount * 0.6 / 100 + 20
     } else {
         0
     }
+    if (amount > 150000 || previousTransfer > 600000) commission = -1
     return commission.toDouble()
 }
 
@@ -49,10 +58,13 @@ fun transferCommission(amount: Int, typeCard: String = "VK Pay", previousTransfe
 }
 
 fun resultPrint(amount: Int, typeCard: String, previousTransfer: Int) {
-    return if (typeCard == "VK Pay" && amount > 15000 || typeCard == "VK Pay" && previousTransfer + amount > 40000
+    return if (resultTransfer(
+            amount,
+            typeCard,
+            previousTransfer
+        ).toInt() == -1
     ) println("Вы превысили лимиты перевода денежных средств")
-    else if (amount <= 150000 || previousTransfer + amount <= 600000
-    ) println(
+    else println(
         "Сумма вашего перевода: $amount рублей\n" +
                 "Перевод с $typeCard\n" +
                 "Итого сумма перевода с учётом комиссии: ${
@@ -64,17 +76,19 @@ fun resultPrint(amount: Int, typeCard: String, previousTransfer: Int) {
                 } рублей\n" +
                 "Сумма переводов за месяц: $previousTransfer рублей"
     )
-    else println("Вы превысили лимиты перевода денежных средств")
+
 }
 
-fun resultTransfer(amount: Int, typeCard: String = "VK Pay", previousTransfer: Int = 0): Double {
-    val result = if (typeCard == "VK Pay" && amount > 15000 || typeCard == "VK Pay" && previousTransfer + amount > 40000
-    )
-        0.0 else if (amount > 150000 || previousTransfer + amount > 600000) 0.0 else amount - transferCommission(
+fun resultTransfer(amount: Int, typeCard: String = "VK Pay", previousTransfer: Int = 0): Number {
+    val result = if (typeCard == "VK Pay" && amount > 15000 || typeCard == "VK Pay" && previousTransfer > 40000
+    ) -1
+    else if (typeMMCommission(amount, previousTransfer).toInt() == -1) -1 else amount - transferCommission(
         amount,
         typeCard,
         previousTransfer
     )
+
+
     return result
 }
 
