8efc01bd715ffa776b33d630fd3d118dc760f01d
Karen.P <karen-pashoyan@mail.ru>
2024-01-19 15:15:41 +0500

    Changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
 package ru.netology
 
 fun main() {
-    var amount = 14000
-    var typeCard = "VK Pay"
+    var amount = 150000
+    var typeCard = "Maestro"
     var previousTransfer = 0
 
     resultTransfer(amount, typeCard, previousTransfer)
@@ -11,38 +11,43 @@ fun main() {
 
     println()
 
-    amount = 15000
-    typeCard = "VK Pay"
-    resultTransfer(amount, typeCard, previousTransfer)
-    previousTransfer += resultTransfer(amount, typeCard, previousTransfer).toInt()
-    resultPrint(amount, typeCard, previousTransfer)
-
-    println()
-
-    amount = 11000
-    typeCard = "VK Pay"
-    resultTransfer(amount, typeCard, previousTransfer)
-    previousTransfer += resultTransfer(amount, typeCard, previousTransfer).toInt()
-    resultPrint(amount, typeCard, previousTransfer)
-
-    println()
-
-    amount = 100000
+    amount = 150000
     typeCard = "Maestro"
     resultTransfer(amount, typeCard, previousTransfer)
     previousTransfer += resultTransfer(amount, typeCard, previousTransfer).toInt()
     resultPrint(amount, typeCard, previousTransfer)
 
+    println()
+
+    amount = 150000
+    typeCard = "Maestro"
+    resultTransfer(amount, typeCard, previousTransfer)
+    previousTransfer += resultTransfer(amount, typeCard, previousTransfer).toInt()
+    resultPrint(amount, typeCard, previousTransfer)
+
+    println()
+
+    amount = 150000
+    typeCard = "Maestro"
+    resultTransfer(amount, typeCard, previousTransfer)
+    previousTransfer += resultTransfer(amount, typeCard, previousTransfer).toInt()
+    resultPrint(amount, typeCard, previousTransfer)
+
+    amount = 150000
+    typeCard = "Maestro"
+    resultTransfer(amount, typeCard, previousTransfer)
+    previousTransfer += resultTransfer(amount, typeCard, previousTransfer).toInt()
+    resultPrint(amount, typeCard, previousTransfer)
 
 }
 
 fun typeMMCommission(amount: Int, previousTransfer: Int): Double {
-    var commission = if (amount > 75000) {
+    var commission = if (amount + previousTransfer > 75000) {
         amount * 0.6 / 100 + 20
     } else {
         0
     }
-    if (amount > 150000 || previousTransfer > 600000) commission = -1
+
     return commission.toDouble()
 }
 
@@ -62,7 +67,7 @@ fun resultPrint(amount: Int, typeCard: String, previousTransfer: Int) {
             amount,
             typeCard,
             previousTransfer
-        ).toInt() == -1
+        ).toInt() == 0
     ) println("Вы превысили лимиты перевода денежных средств")
     else println(
         "Сумма вашего перевода: $amount рублей\n" +
@@ -80,9 +85,10 @@ fun resultPrint(amount: Int, typeCard: String, previousTransfer: Int) {
 }
 
 fun resultTransfer(amount: Int, typeCard: String = "VK Pay", previousTransfer: Int = 0): Number {
-    val result = if (typeCard == "VK Pay" && amount > 15000 || typeCard == "VK Pay" && previousTransfer > 40000
-    ) -1
-    else if (typeMMCommission(amount, previousTransfer).toInt() == -1) -1 else amount - transferCommission(
+    val result = if (typeCard == "VK Pay" && previousTransfer > 40000 || typeCard == "VK Pay" && amount > 15000
+    ) 0
+    else if (amount > 150000 || previousTransfer > 600000) 0
+    else amount - transferCommission(
         amount,
         typeCard,
         previousTransfer
